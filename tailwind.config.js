d89b95db8dd5a60d1e949d7d89e420db428fbd7e
pa3k-tabs <epoxide7@gmail.com>
2023-12-05 16:52:16 +0100

    Futminna website clone

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        futminna: "#800e80",
+        bgemp: "#4c2849",
+      },
+      textColor: {
+        cpess: "#2d4167",
+        futminna: "#800e80",
+      },
+      borderColor: {
+        futminna: "#800e80",
+      },
+    },
+  },
+  plugins: [],
+};
